8568a1e14aa002053b75e1fcea01c1db0205400b
Abbas Jalali <Jalali@Simataksun.local>
2025-12-30 10:37:56 +0330

    feat(i18n): add Persian (fa) localization

--- src/core/i18n/config.ts
+++ src/core/i18n/config.ts
@@ -2,4 +2,4 @@ export const defaultLocale = 'en'
 
 export const timeZone = 'Europe/Amsterdam'
 
-export const locales = [defaultLocale, 'ru', 'zh'] as const
+export const locales = [defaultLocale, 'ru', 'zh', 'fa'] as const
